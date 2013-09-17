099ead15f5ebb8fa4abf3bf35410e568eded0a79
Debbie Reches <dreches@fas.harvard.edu>
2013-09-17 15:23:38 -0400

    Adding an image to the page

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+</head>
 <body>
-<h1>This is project 1</h1>
-<p>Debbie Reches</p>
+<h3>Project 1</h3>
+<h2>Debbie Reches</h2>
+<h2>I volunteered for the Elizabeth Warren campaign. Mostly canvassing.</h2>
+		<a href="http://elizabethwarren.com">My son, me and E. Warren 
+		<img src="images/ewarren.jpg" alt="ewarren" width="400" height="300"/></a>
+
+		<h2>Hobbies</h2>
+		
+				<ul>
+		<li><a href="http://www.youtube.com/watch?v=zItilwTX7IQ&amp;feature=player_embedded">Israeli dancing </a></li>
+        <li>crossword puzzles</li>
+		<li>taking classes</li>
+			</ul>
+<h4>Recent coursework</h4>
 <ul>
 <li>CSCIE12
+</li>
+<li>CSCIE147
 </li>
 </ul>
 </body>
