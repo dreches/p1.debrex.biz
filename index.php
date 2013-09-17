e32b5b7c18f1e44b001aefbf847c46d45ba1f486
Debbie Reches <dreches@fas.harvard.edu>
2013-09-17 15:48:57 -0400

    added editor

--- index.php
+++ index.php
@@ -3,19 +3,20 @@
 <head>
 </head>
 <body>
-<h3>Project 1</h3>
-<h2>Debbie Reches</h2>
-<h2>I volunteered for the Elizabeth Warren campaign. Mostly canvassing.</h2>
+<h2>Project 1</h2>
+<h3>Name: Debbie Reches</h3>
+<h3>Editor: Notepad++</h3>
+<h3>Hobbies</h3>
+<ul>
+<li>
+<p>I volunteered for the Elizabeth Warren campaign. Mostly canvassing.</p>
 		<a href="http://elizabethwarren.com">My son, me and E. Warren 
 		<img src="images/ewarren.jpg" alt="ewarren" width="400" height="300"/></a>
 
-		<h2>Hobbies</h2>
-		
-				<ul>
+</li>	
 		<li><a href="http://www.youtube.com/watch?v=zItilwTX7IQ&amp;feature=player_embedded">Israeli dancing </a></li>
         <li>crossword puzzles</li>
-		<li>taking classes</li>
-			</ul>
+	</ul>
 <h4>Recent coursework</h4>
 <ul>
 <li>CSCIE12
