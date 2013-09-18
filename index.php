ead3b5623a88f76d0365d13ec5e806419f0daf46
Debbie Reches <dreches@fas.harvard.edu>
2013-09-17 22:32:52 -0400

    made a small text change

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <fieldset>
 <legend>Background</legend>
 <p>
-Over the course of over a decade+, I have taken several courses at Harvard Extension school, including CSCIE12 last semester. Over the summer
+Over the course of over a decade, I have taken several courses at Harvard Extension school, including CSCIE12 last semester. Over the summer
 my CSS/Javascript skills have managed to get rusty, and I hope to refresh them in this class. 
 I had already taken a few courses on XML, including Web Development using XML and Developing e-Business applications using XML and Java.
 I was a TA for the latter class as well, (in 2002), along with then graduate student David Malan, who now teaches at Harvard. 
