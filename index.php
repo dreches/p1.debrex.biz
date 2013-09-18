c5f60e92619f72441fa172ed894672703e82e224
Debbie Reches <dreches@fas.harvard.edu>
2013-09-17 22:28:34 -0400

    adding css styling

--- index.php
+++ index.php
@@ -1,27 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="aboutme.css"/>
 </head>
 <body>
+<form id="innerpage">
 <h2>Project 1</h2>
+<fieldset>
+<legend>Data</legend>
 <h3>Name: Debbie Reches</h3>
 <h3>Editor: Notepad++</h3>
-<h3>Hobbies</h3>
+</fieldset>
+<fieldset>
+<legend>Background</legend>
+<p>
+Over the course of over a decade+, I have taken several courses at Harvard Extension school, including CSCIE12 last semester. Over the summer
+my CSS/Javascript skills have managed to get rusty, and I hope to refresh them in this class. 
+I had already taken a few courses on XML, including Web Development using XML and Developing e-Business applications using XML and Java.
+I was a TA for the latter class as well, (in 2002), along with then graduate student David Malan, who now teaches at Harvard. 
+
+I worked till recently at MEDITECH, for 13 years, in their interface/integration group. There is definitely a lot of room for improvement
+when it comes to Hospital Information Systems. Our fragmented healthcare system, and fundamental distrust of consolidating patient data by any
+one group, and recently passed heavy handed patient privacy regulations have definitely slowed down the delivery of modern and effective software
+and data analysis to our healthcare delivery system. For example, just verifying that you are dealing with one and the same patient in two different systems is
+a challenge, because we do not have a universal patient id. But some of that is changing. 
+</p>
+</fieldset>
+<fieldset>
+<legend>Hobbies</legend>
 <ul>
 <li>
-<p>I volunteered for the Elizabeth Warren campaign. Mostly canvassing.</p>
+<p>Politics. I volunteered for the Elizabeth Warren campaign. Mostly canvassing.</p>
 		<a href="http://elizabethwarren.com">My son, me and E. Warren 
 		<img src="images/ewarren.jpg" alt="ewarren" width="400" height="300"/></a>
 
 </li>	
 		<li><a href="http://www.youtube.com/watch?v=zItilwTX7IQ&amp;feature=player_embedded">Israeli dancing </a></li>
-        <li>crossword puzzles</li>
+        <li>Crossword Puzzles</li>
+		<li>
+		NY Times (I am originally from NY). Especially online, they have great blogs.
+		</li>
 	</ul>
-<h4>Recent coursework</h4>
-<ul>
-<li>CSCIE12
-</li>
-<li>CSCIE147
-</li>
-</ul>
+</fieldset>
+</form>
 </body>
